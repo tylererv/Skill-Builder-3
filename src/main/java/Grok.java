cbbf23c85507ae5d0c1e77894ebfeb8d926aaec3
tylererv <tylerervin193176@yahoo.com>
2024-02-14 17:22:35 -0500

    Completed Version

--- src/main/java/Grok.java
+++ src/main/java/Grok.java
@@ -9,5 +9,61 @@
  */
 public class Grok
 {
-    // implement this class per instructions in Required Activities.
+    private static final int DEFAULT_POWER_LEVEL = 50;
+    private int powerLevel;
+
+    /**
+     \* Initializes a Grok object to the default power level of 50.
+     */
+    public Grok()
+    {
+        powerLevel = DEFAULT_POWER_LEVEL;
+    }
+
+    /**
+     *
+     * Initializes a Grok object to power powerLevel
+     */
+    public Grok(int power)
+    {
+        powerLevel = power;
+    }
+
+    /*
+     * Returns the power level of this Grok.
+     * @return returns the power level of this Grok
+     */
+
+    public int getPowerLevel()
+    {
+        return powerLevel;
+    }
+
+    /*
+     * Sets the power level of this Grok.
+     * @param powerLevel the power value to set for this Grok.
+     */
+    public void setPowerLevel(int powerLevel)
+    {
+        this.powerLevel = powerLevel;
+    }
+
+    /** Set the power level of this Grok to the power level of the pill.
+  * @param pill The PowerPill that the this Grok. The power
+  * of the pill is added to the power level of this Grok.
+  */
+
+    public void takePowerPill(PowerPill pill)
+    {
+        powerLevel += pill.getPower();
+    }
+
+    /*
+     * Invoked when this Grok takes a hit.  The power level of
+     * this Grok is reduced by 5.
+     */
+    public void tookHit()
+    {
+        powerLevel -= 5;
+    }
 }
